beca1e435ea92a46d61466f46ed5098103dbbbf6
hilmizul <netspytux@gmail.com>
2026-05-13 14:19:51 +0700

    create migration: back to select type for field(triwulan)

---
+++ pb_migrations/1778656577_updated_aset.js
@@ -0,0 +1,30 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3183783002")
+
+  // remove field
+  collection.fields.removeById("text2567345951")
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3183783002")
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text2567345951",
+    "max": 0,
+    "min": 0,
+    "name": "triwulan",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
