7f64cde36756cdf0eb9d5c7b6d53563d16752bc4
hilmizul <netspytux@gmail.com>
2026-06-11 01:52:33 +0700

    update record and manage rules user API

---
+++ pb_migrations/1781116884_updated_users.js
@@ -0,0 +1,22 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "manageRule": "@request.auth.collectionName = 'users'",
+    "updateRule": "@request.auth.id != ''"
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "manageRule": null,
+    "updateRule": "@request.auth.id != '' || @request.auth.collectionName = 'users'"
+  }, collection)
+
+  return app.save(collection)
+})
